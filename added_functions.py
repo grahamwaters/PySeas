57632e5529b04e015ee5b1e5586aaaff0ef16fa3
Graham Waters <graham.waters.business@gmail.com>
2022-11-05 21:40:36 -0500

    cams: Updated

--- added_functions.py
+++ added_functions.py
@@ -1,16 +1,18 @@
 import pandas as pd
+import daylight
 
 
 
 
 # The end goal is to be able to classify images from the buoy cameras in real time as either:
-# (1) Beautiful
+# (1) Beautiful - high contrast and high saturation images
 # (2) Unuseable (due to bad weather)
 # (3) Unuseable (due to bad camera angle)
 # (4) Unuseable (due to dark conditions)
-# (5) Sunset shot
-# (6) Sunrise shot
+# (5) Sunset shot - will have a higher yellow/red/pink color value than usual.
+# (6) Sunrise shot - will have a higher red, and orange color value than usual.
 # (7) Useable for a Panorama
+# A set of photos is useful for a panorama if the photos have a similar angle. This is useful for the panorama stitching software to work properly.
 # (8) Contains a boat
 # (9) Contains a person
 # (10) Contains a bird
